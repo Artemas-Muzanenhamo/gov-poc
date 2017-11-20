4407fbf90efb40866ef9a08bc4c5533f2451de14
Artemas <artemasseng@gmail.com>
2017-11-20 11:59:27 +0000

    Added a Test to findAllIdentities. [INCOMPLETE]

--- identity-service/src/test/java/com/gov/zw/repository/IdentityRepositoryTest.java
+++ identity-service/src/test/java/com/gov/zw/repository/IdentityRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -29,6 +31,42 @@ public class IdentityRepositoryTest {
         Assertions.assertThat(identities.get(0).getName()).isEqualTo("Artemas");
         Assertions.assertThat(identities.get(0).getSurname()).isEqualTo("Muzanenhamo");
 
+    }
+
+    @Test
+    public void findAllIdentities(){
+        List<Identity> identityList = new ArrayList<>();
+
+//        List<Identity> identities = new ArrayList<>(Arrays.asList(
+//                new Identity("", "Artemas", "Muzanenhamo", "28/03/1990",
+//                        "Mashayamombe", "Harare", "17/11/2017"),
+//                new Identity("", "Terrence", "Munhengu", "15/04/1980",
+//                        "Murehwa", "Mutare", "17/11/2017"),
+//                new Identity("", "Tichaona", "Chimuchero", "07/12/1960",
+//                        "Gutu", "Goromhonzi", "17/11/2017"),
+//                new Identity("", "Zindoga", "Ncube", "08/01/1976",
+//                        "Mhondoro", "Harare", "17/11/2017")
+//        ));
+//
+//        identities.forEach(t -> this.repository.save(t));
+
+        this.repository.save(
+                new Identity("", "Artemas", "Muzanenhamo", "28/03/1990",
+                        "Mashayamombe", "Harare", "17/11/2017"));
+        this.repository.save(
+                new Identity("", "Terrence", "Munhengu", "15/04/1980",
+                        "Murehwa", "Mutare", "17/11/2017"));
+        this.repository.save(
+                new Identity("", "Tichaona", "Chimuchero", "07/12/1960",
+                        "Gutu", "Goromhonzi", "17/11/2017"));
+        this.repository.save(
+                new Identity("", "Zindoga", "Ncube", "08/01/1976",
+                        "Mhondoro", "Harare", "17/11/2017"));
+        identityList.addAll(this.repository.findAll());
+        System.out.println(identityList.get(0).getBirthDate());
+        Assertions.assertThat(identityList.size()).isEqualTo(4);
+        Assertions.assertThat(identityList.get(0).getSurname()).isEqualTo("Muzanenhamo");
+        Assertions.assertThat(identityList.get(3).getSurname()).isEqualTo("Ncube");
     }
 
 }
