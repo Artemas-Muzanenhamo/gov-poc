1b3b8328e0554d270fcab2e81cd113a06dbf7b96
Artemas-Muzanenhamo <artemasseng@gmail.com>
2017-11-21 19:26:38 +0000

    Added License Service

---
+++ license-service/src/main/java/com/gov/zw/LicenseServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gov.zw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseServiceApplication.class, args);
+	}
+}
