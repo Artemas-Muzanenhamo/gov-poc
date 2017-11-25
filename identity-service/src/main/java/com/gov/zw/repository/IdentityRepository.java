e3fb545535b11a8d74033b193f78f153c2698cdd
Artemas-Muzanenhamo <artemasseng@gmail.com>
2017-11-25 16:38:50 +0000

    Added RestResource on the Identity-Service Repo

--- identity-service/src/main/java/com/gov/zw/repository/IdentityRepository.java
+++ identity-service/src/main/java/com/gov/zw/repository/IdentityRepository.java
@@ -2,11 +2,16 @@ package com.gov.zw.repository;
 
 import com.gov.zw.domain.Identity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface IdentityRepository extends MongoRepository<Identity, String> {
 
-    List<Identity> findIdentitiesByName(String name);
+    @RestResource(path = "by-name")
+    List<Identity> findIdentitiesByName(@Param("rn") String name);
 
 }
