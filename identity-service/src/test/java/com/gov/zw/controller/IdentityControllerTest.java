03a88d631026e08a48fcfb4c97f0e94db04fd432
Artemas <artemasseng@gmail.com>
2017-12-15 12:11:27 +0000

    Polish up

--- identity-service/src/test/java/com/gov/zw/controller/IdentityControllerTest.java
+++ identity-service/src/test/java/com/gov/zw/controller/IdentityControllerTest.java
@@ -2,12 +2,13 @@ package com.gov.zw.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gov.zw.domain.Identity;
+import com.gov.zw.repository.IdentityRepository;
 import net.minidev.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,16 +17,17 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
+@WebMvcTest(IdentityController.class)
 public class IdentityControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private IdentityRepository identityRepository;
 
     @Test
     public void saveIdentity() throws Exception {
--- license-service/src/test/java/com/gov/zw/controller/LicenseControllerTest.java
+++ license-service/src/test/java/com/gov/zw/controller/LicenseControllerTest.java
@@ -2,12 +2,13 @@ package com.gov.zw.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gov.zw.domain.License;
+import com.gov.zw.repository.LicenseRepository;
 import net.minidev.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,13 +18,15 @@ import java.util.Map;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
+@WebMvcTest(LicenseController.class)
 public class LicenseControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private LicenseRepository licenseRepository;
 
     @Test
     public void addLicense() throws Exception {
