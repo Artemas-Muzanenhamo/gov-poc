df6703e0e846506b477f2d62f1718686abf872a4
Artemas <artemasseng@gmail.com>
2017-12-28 16:32:30 +0000

    Fix License-Service

--- license-service/src/main/java/com/gov/zw/LicenseServiceApplication.java
+++ license-service/src/main/java/com/gov/zw/LicenseServiceApplication.java
@@ -4,11 +4,8 @@ import com.gov.zw.configuration.HelloRibbonClientConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-<<<<<<< HEAD
-=======
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
->>>>>>> d9dea1dee7fd33c1d6bc52f2d3dd409675c4396a
 
 @EnableEurekaClient
 @SpringBootApplication
