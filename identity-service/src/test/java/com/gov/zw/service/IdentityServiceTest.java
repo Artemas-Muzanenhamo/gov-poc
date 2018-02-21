6ba9c431deb094d3c24254ec7bf984d5ac9be8a3
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-02-21 14:10:21 +0000

    add valid identity details test case

--- identity-service/src/test/java/com/gov/zw/service/IdentityServiceTest.java
+++ identity-service/src/test/java/com/gov/zw/service/IdentityServiceTest.java
@@ -1,19 +1,25 @@
 package com.gov.zw.service;
 
 import com.gov.zw.domain.Identity;
+import com.gov.zw.repository.IdentityRepository;
 import com.gov.zw.util.InvalidIdentityException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 public class IdentityServiceTest {
 
     @InjectMocks
-    IdentityServiceImpl identityService;
+    private IdentityServiceImpl identityService;
+
+    @Mock
+    private IdentityRepository identityRepository;
 
     @Test(expected = InvalidIdentityException.class)
     public void should_throw_an_exception_when_an_invalid_identity_is_passed() throws Exception {
@@ -22,5 +28,9 @@ public class IdentityServiceTest {
 
     @Test
     public void should_save_identity_if_identity_details_exist() throws InvalidIdentityException {
+        Identity identity = new Identity("1", "1", "Artemas", "Muzanenhamo", "28/03/1990",
+                "Mashayamombe", "Harare", "17/11/2017");
+        identityService.save(identity);
+        verify(identityRepository, times(1)).save(identity);
     }
 }
