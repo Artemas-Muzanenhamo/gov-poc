f3ebc58a5c0fc56868352a69cd92e446ecc45a36
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-02-26 11:44:31 +0000

    polish up

--- discovery-server/src/main/java/com/gov/zw/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/gov/zw/DiscoveryServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
 }
