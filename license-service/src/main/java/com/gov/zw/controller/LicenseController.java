2a1abf7b04e7bb9c7726f347536ef9b126490da2
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-02-26 11:23:14 +0000

    polish up

--- license-service/src/main/java/com/gov/zw/controller/LicenseController.java
+++ license-service/src/main/java/com/gov/zw/controller/LicenseController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class LicenseController {
     private final LicenseService licenseServiceImpl;
 
-    public LicenseController(LicenseService licenseServiceImpl){
+    public LicenseController(LicenseService licenseServiceImpl) {
         this.licenseServiceImpl = licenseServiceImpl;
     }
 
