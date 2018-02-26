3c59c430dd0e3904ffd1edb84c75d667c26f4a66
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-02-26 11:37:11 +0000

    polish up

--- identity-service/src/main/java/com/gov/zw/service/IdentityService.java
+++ identity-service/src/main/java/com/gov/zw/service/IdentityService.java
@@ -7,10 +7,25 @@ import com.gov.zw.util.InvalidIdentityReferenceException;
 
 import java.util.List;
 
+/**
+ * This Service allows you to perform all the CRUD
+ * operations for an {@link Identity}.
+ */
 public interface IdentityService {
 
+    /**
+     * Allows you to save an {@link Identity}.
+     * @param identity - {@link Identity} to be saved.
+     * @throws InvalidIdentityException
+     */
     void save(Identity identity) throws InvalidIdentityException;
 
+    /**
+     * Allows you to find an {@link Identity} by name.
+     * @param name - Name of the {@link Identity} to be returned.
+     * @return - a list of Identities.
+     * @throws InvalidIdentityNameException
+     */
     List<Identity> findIdentitiesByName(String name) throws InvalidIdentityNameException;
 
     Identity findIdentityByIdentityRef(String idRef) throws InvalidIdentityReferenceException;
