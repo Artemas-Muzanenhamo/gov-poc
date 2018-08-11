6264993b911270040ceae40f6d8df8c9a52d3142
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-08-12 00:35:58 +0100

    polish up

--- identity-service/src/test/java/com/gov/zw/IdentityServiceApplicationTests.java
+++ identity-service/src/test/java/com/gov/zw/IdentityServiceApplicationTests.java
@@ -1,6 +1,6 @@
 package com.gov.zw;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
