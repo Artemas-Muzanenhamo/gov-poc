ebf00c67f77002de7e5c6627bbd1da6c7b83e68a
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-08-28 16:05:10 +0100

    amend IdentityClient to use @Component annotation

--- license-service/src/main/java/com/gov/zw/client/IdentityClient.java
+++ license-service/src/main/java/com/gov/zw/client/IdentityClient.java
@@ -2,12 +2,14 @@ package com.gov.zw.client;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
 
 @FeignClient(name = "identity-service")
+@Component
 public interface IdentityClient {
 
     @PostMapping(value = "/identities/reference")
