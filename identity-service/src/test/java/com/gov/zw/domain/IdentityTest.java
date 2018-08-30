ff833df27265cc2c8bc968d1d175014cac32db96
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-08-30 09:38:36 +0100

    add more unit tests for the Identity Model

--- identity-service/src/test/java/com/gov/zw/domain/IdentityTest.java
+++ identity-service/src/test/java/com/gov/zw/domain/IdentityTest.java
@@ -1,22 +1,71 @@
 package com.gov.zw.domain;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class IdentityTest {
 
-    @Test
-    void creation(){
-        Identity identity = new Identity(
+    private Identity identity;
+    private Identity identity2;
+
+    @BeforeEach
+    void init() {
+        identity = new Identity(
                 "1", "1234AM1", "Artemas", "Muzanenhamo", "28/03/1990", "Mashayamombe",
                 "Harare", "22/11/2017");
 
-        Assertions.assertThat(identity.getIdentityRef()).isEqualTo("1234AM1");
-        Assertions.assertThat(identity.getName()).isEqualTo("Artemas");
-        Assertions.assertThat(identity.getSurname()).isEqualTo("Muzanenhamo");
-        Assertions.assertThat(identity.getBirthDate()).isEqualTo("28/03/1990");
-        Assertions.assertThat(identity.getVillageOfOrigin()).isEqualTo("Mashayamombe");
-        Assertions.assertThat(identity.getPlaceOfBirth()).isEqualTo("Harare");
-        Assertions.assertThat(identity.getDateOfIssue()).isEqualTo("22/11/2017");
+        identity2 = new Identity();
     }
+
+    @Test
+    void creation(){
+        assertThat(identity.getId()).isEqualTo("1");
+        assertThat(identity.getIdentityRef()).isEqualTo("1234AM1");
+        assertThat(identity.getName()).isEqualTo("Artemas");
+        assertThat(identity.getSurname()).isEqualTo("Muzanenhamo");
+        assertThat(identity.getBirthDate()).isEqualTo("28/03/1990");
+        assertThat(identity.getVillageOfOrigin()).isEqualTo("Mashayamombe");
+        assertThat(identity.getPlaceOfBirth()).isEqualTo("Harare");
+        assertThat(identity.getDateOfIssue()).isEqualTo("22/11/2017");
+    }
+
+    @Test
+    void noArgsEquals(){
+        Identity expectedId = new Identity();
+        assertThat(expectedId).isEqualTo(identity2);
+    }
+
+    @Test
+    void noArgsNotEquals(){
+        Identity expectedId = new Identity();
+        assertThat(expectedId).isNotEqualTo(identity);
+    }
+
+    @Test
+    void hashCodeNotEquals(){
+        Identity expectedId = new Identity();
+        assertThat(expectedId.hashCode()).isNotEqualTo(identity.hashCode());
+    }
+
+    @Test
+    void hashCodeEquals(){
+        Identity expectedId = identity;
+        assertThat(expectedId.hashCode()).isEqualTo(identity.hashCode());
+    }
+
+    @Test
+    void toStringNotEquals(){
+        Identity expectedId = new Identity();
+        assertThat(expectedId.toString()).isNotEqualTo(identity.toString());
+    }
+
+    @Test
+    void toStringEquals(){
+        Identity expectedId = new Identity();
+        assertThat(expectedId.toString()).isEqualTo(identity2.toString());
+    }
+
+
 }
