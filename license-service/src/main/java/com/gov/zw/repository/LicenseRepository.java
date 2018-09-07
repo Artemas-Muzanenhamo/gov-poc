5e4d3334be8cdd5d454232be4352a0957495dd1c
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-09-06 23:24:02 -1100

    add custom query in LicenseRepository

--- license-service/src/main/java/com/gov/zw/repository/LicenseRepository.java
+++ license-service/src/main/java/com/gov/zw/repository/LicenseRepository.java
@@ -2,6 +2,10 @@ package com.gov.zw.repository;
 
 import com.gov.zw.domain.License;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LicenseRepository extends MongoRepository<License, String> {
+    List<License> findLicensesByFirstNames(@Param("name") String name);
 }
