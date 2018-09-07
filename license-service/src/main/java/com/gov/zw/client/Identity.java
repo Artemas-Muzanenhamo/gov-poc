9bd1b5867c0ab47e39daa2625904aaf643b0ec58
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-09-06 23:21:01 -1100

    add lombok support to Identity Client

--- license-service/src/main/java/com/gov/zw/client/Identity.java
+++ license-service/src/main/java/com/gov/zw/client/Identity.java
@@ -1,7 +1,14 @@
 package com.gov.zw.client;
 
+import lombok.*;
+
 import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Identity {
 
     private String id;
@@ -12,70 +19,4 @@ public class Identity {
     private String villageOfOrigin;
     private String placeOfBirth;
     private String dateOfIssue;
-
-    public Identity() {
-    }
-
-    public Identity(String id, String identityRef, String name, String surname, String birthDate, String villageOfOrigin, String placeOfBirth, String dateOfIssue) {
-        this.id = id;
-        this.identityRef = identityRef;
-        this.name = name;
-        this.surname = surname;
-        this.birthDate = birthDate;
-        this.villageOfOrigin = villageOfOrigin;
-        this.placeOfBirth = placeOfBirth;
-        this.dateOfIssue = dateOfIssue;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getIdentityRef() {
-        return identityRef;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public String getVillageOfOrigin() {
-        return villageOfOrigin;
-    }
-
-    public String getPlaceOfBirth() {
-        return placeOfBirth;
-    }
-
-    public String getDateOfIssue() {
-        return dateOfIssue;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Identity)) return false;
-        Identity identity = (Identity) o;
-        return Objects.equals(id, identity.id) &&
-                Objects.equals(identityRef, identity.identityRef) &&
-                Objects.equals(name, identity.name) &&
-                Objects.equals(surname, identity.surname) &&
-                Objects.equals(birthDate, identity.birthDate) &&
-                Objects.equals(villageOfOrigin, identity.villageOfOrigin) &&
-                Objects.equals(placeOfBirth, identity.placeOfBirth) &&
-                Objects.equals(dateOfIssue, identity.dateOfIssue);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, identityRef, name, surname, birthDate, villageOfOrigin, placeOfBirth, dateOfIssue);
-    }
 }
