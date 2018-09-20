099a46b32198f3e31b3f7c3a8d377b91e411e358
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2018-09-20 08:02:42 +0100

    add more data in DBseeder

--- license-service/src/main/java/com/gov/zw/DBSeeder.java
+++ license-service/src/main/java/com/gov/zw/DBSeeder.java
@@ -5,6 +5,9 @@ import com.gov.zw.repository.LicenseRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class DBSeeder implements CommandLineRunner{
 
@@ -16,15 +19,34 @@ public class DBSeeder implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        License license = new License("MUZAN1234", "121", "Muzanenhamo", "Artemas",
-                "28/03/1990", "United Kingdom", "28/03/2010",
-                "28/03/2060", "DVLA", "MUZANK9843ACTK", "001.jpg",
-                "27 Foxhill Street, Guildford, Surrey, GU21 9EE");
+
+        List<License> licenses = Arrays.asList(
+                new License("MUZAN1234", "121", "Muzanenhamo", "Artemas",
+                        "28/03/1990", "United Kingdom", "28/03/2010",
+                        "28/03/2060", "DVLA", "MUZANK9843ACTK", "001.jpg",
+                        "27 Foxhill Street, Guildford, Surrey, GU21 9EE"),
+                new License("TONYM1234", "131", "Morretti", "Thomas",
+                        "15/01/1980", "United Kingdom", "28/03/2010",
+                        "28/03/2060", "DVLA", "MUZANK9843ACTK", "001.jpg",
+                        "27 Foxhill Street, Guildford, Surrey, GU21 9EE"),
+                new License("MATTS1234", "141", "Smith", "Matthew",
+                        "14/08/1950", "United Kingdom", "28/03/2010",
+                        "28/03/2060", "DVLA", "MUZANK9843ACTK", "001.jpg",
+                        "27 Foxhill Street, Guildford, Surrey, GU21 9EE"),
+                new License("JOHW1234", "151", "Walter", "John",
+                        "28/03/1990", "United Kingdom", "28/03/2010",
+                        "28/03/2060", "DVLA", "MUZANK9843ACTK", "001.jpg",
+                        "27 Foxhill Street, Guildford, Surrey, GU21 9EE"),
+                new License("ROBJ1234", "161", "Jenkins", "Rob",
+                        "28/03/1990", "United Kingdom", "28/03/2010",
+                        "28/03/2060", "DVLA", "MUZANK9843ACTK", "001.jpg",
+                        "27 Foxhill Street, Guildford, Surrey, GU21 9EE")
+        );
 
         // delete all data before we put data to use
         this.licenseRepository.deleteAll();
 
         // add dummy data
-        this.licenseRepository.save(license);
+        this.licenseRepository.saveAll(licenses);
     }
 }
