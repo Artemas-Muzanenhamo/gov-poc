846b3c5796e06ba6b0b339468daf9469ae006363
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2018-09-27 19:18:58 +0100

    add more meaningful data to DBSeeder

--- identity-service/src/main/java/com/gov/zw/DBSeeder.java
+++ identity-service/src/main/java/com/gov/zw/DBSeeder.java
@@ -22,13 +22,13 @@ public class DBSeeder implements CommandLineRunner {
         List<Identity> identity = Arrays.asList(
                 new Identity("1", "121", "Artemas", "Muzanenhamo", "28/03/1990",
                 "Mashayamombe", "Harare", "17/11/2017"),
-                new Identity("2", "122", "Artemas", "Muzanenhamo", "28/03/1990",
-                        "Mashayamombe", "Harare", "17/11/2017"),
-                new Identity("3", "123", "Artemas", "Muzanenhamo", "28/03/1990",
+                new Identity("2", "122", "Thomas", "Smith", "02/05/1950",
+                        "Surrey", "Harare", "17/11/2017"),
+                new Identity("3", "123", "Mark", "Jefferson", "17/01/1995",
                 "Mashayamombe", "Harare", "17/11/2017"),
-                new Identity("4", "124", "Artemas", "Muzanenhamo", "28/03/1990",
+                new Identity("4", "124", "Jess", "Carrol", "28/03/1990",
                 "Mashayamombe", "Harare", "17/11/2017"),
-                new Identity("5", "125", "Artemas", "Muzanenhamo", "28/03/1990",
+                new Identity("5", "125", "Matthew", "Johnson", "28/03/1990",
                 "Mashayamombe", "Harare", "17/11/2017")
         );
 
