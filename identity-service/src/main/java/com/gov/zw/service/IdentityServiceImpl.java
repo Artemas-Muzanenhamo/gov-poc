50df35280640e2710379dbbb6aca8b8f61074e55
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-09-30 00:17:09 +0100

    polish up

--- identity-service/src/main/java/com/gov/zw/service/IdentityServiceImpl.java
+++ identity-service/src/main/java/com/gov/zw/service/IdentityServiceImpl.java
@@ -22,31 +22,22 @@ public class IdentityServiceImpl implements IdentityService {
     @Override
     public void save(Identity identity) throws InvalidIdentityException {
         Optional<Identity> identityOptional = Optional.ofNullable(identity);
-        if (identityOptional.isPresent()) {
-            identityRepository.save(identity);
-        } else {
-            throw new InvalidIdentityException("The Identity is invalid!");
-        }
+        identityRepository.save(identityOptional
+                .orElseThrow( () -> new InvalidIdentityException("The Identity is invalid!")));
     }
 
     @Override
     public List<Identity> findIdentitiesByName(String name) throws InvalidIdentityNameException {
         Optional<String> nameOptional = Optional.ofNullable(name);
-        if (nameOptional.isPresent()) {
-            return identityRepository.findIdentitiesByName(name);
-        } else {
-            throw new InvalidIdentityNameException("The name supplied does not exist!");
-        }
+        return identityRepository.findIdentitiesByName(nameOptional
+                .orElseThrow( () -> new InvalidIdentityNameException("The name supplied does not exist!")));
     }
 
     @Override
     public Identity findIdentityByIdentityRef(String idRef) throws InvalidIdentityReferenceException {
         Optional<String> idReferenceOptional = Optional.ofNullable(idRef);
-        if (idReferenceOptional.isPresent()) {
-            return identityRepository.findIdentityByIdentityRef(idRef);
-        } else {
-            throw new InvalidIdentityReferenceException("The ID reference supplied is not valid!");
-        }
+        return identityRepository.findIdentityByIdentityRef(idReferenceOptional
+                .orElseThrow( () -> new InvalidIdentityReferenceException("The ID reference supplied is not valid!")));
     }
 
     @Override
@@ -57,10 +48,7 @@ public class IdentityServiceImpl implements IdentityService {
     @Override
     public void delete(Identity identity) throws InvalidIdentityException {
         Optional<Identity> identityOptional = Optional.ofNullable(identity);
-        if (identityOptional.isPresent()) {
-            identityRepository.delete(identity);
-        } else {
-            throw new InvalidIdentityException("The Identity to be deleted is invalid!");
-        }
+        identityRepository.delete(identityOptional
+                .orElseThrow(() -> new InvalidIdentityException("The Identity to be deleted is invalid!")));
     }
 }
