dd9318d979a7f926625371fd5a50ca2ada08f6eb
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2018-10-02 17:08:01 +0100

    add custom query for IdentityService

--- identity-service/src/main/java/com/gov/zw/repository/IdentityRepository.java
+++ identity-service/src/main/java/com/gov/zw/repository/IdentityRepository.java
@@ -2,6 +2,7 @@ package com.gov.zw.repository;
 
 import com.gov.zw.domain.Identity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -13,6 +14,6 @@ public interface IdentityRepository extends MongoRepository<Identity, String> {
     List<Identity> findIdentitiesBySurname(String surname);
     List<Identity> findIdentitiesByVillageOfOrigin(String villageOfOrigin);
     List<Identity> findIdentitiesByNameAndSurnameAndVillageOfOrigin(String name, String surname, String villageOfOrigin);
-
+    Identity findIdentityByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
     Identity findIdentityByIdentityRef(String identityReference);
 }
