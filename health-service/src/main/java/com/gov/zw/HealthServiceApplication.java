f56435232eed9cfbb5bb86480d6bf0ed9d534ef0
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-10-15 22:44:07 +0100

    add Health-Service microservice

---
+++ health-service/src/main/java/com/gov/zw/HealthServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gov.zw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthServiceApplication.class, args);
+	}
+}
