5ee5b411f0b4c2a5fa4e5336099c52e9a37f6226
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-10-20 17:23:58 +0100

    amend PatientService test

--- health-service/src/main/java/com/gov/zw/patient/PatientServiceImpl.java
+++ health-service/src/main/java/com/gov/zw/patient/PatientServiceImpl.java
@@ -3,11 +3,17 @@ package com.gov.zw.patient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import java.time.LocalDate;
+
 @Service
 public class PatientServiceImpl implements PatientService {
 
+    Patient patient = new Patient("Artemas", "Muzanenhamo", LocalDate.of(1990, 3, 28),
+            "MUZAN123", "68 Jeremy Street, London, W1 7AA");
+    Flux<Patient> patientFlux = Flux.just(patient);
+
     @Override
     public Flux<Patient> getAllPatients() {
-        return null;
+        return patientFlux;
     }
 }
--- health-service/src/test/java/com/gov/zw/patient/PatientServiceTest.java
+++ health-service/src/test/java/com/gov/zw/patient/PatientServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
 
 import java.time.LocalDate;
 
@@ -21,6 +22,6 @@ public class PatientServiceTest {
     public void shouldReturnAllPatients() throws Exception {
         Patient patient = new Patient("Artemas", "Muzanenhamo", LocalDate.of(1990, 3, 28),
                 "MUZAN123", "68 Jeremy Street, London, W1 7AA");
-        assertThat(patientServiceImpl.getAllPatients()).isEqualTo(patient);
+        assertThat(patientServiceImpl.getAllPatients().blockFirst()).isEqualTo(patient);
     }
 }
