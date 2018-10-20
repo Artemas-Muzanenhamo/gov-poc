dcd21ce1bad1e09dc86a2cb6b13661b5aeda81de
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-10-20 23:45:43 +0100

    add PatientReactiveMongo

--- health-service/src/main/java/com/gov/zw/patient/PatientRepository.java
+++ health-service/src/main/java/com/gov/zw/patient/PatientRepository.java
@@ -1,6 +1,8 @@
 package com.gov.zw.patient;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository extends ReactiveMongoRepository<Patient, String> {
 }
