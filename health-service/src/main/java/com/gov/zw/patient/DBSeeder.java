7d39346fd5e76049c5cbc196eb9e60961dd5a4a1
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-10-27 23:44:53 +0100

    add DBSeeder for Patient-Service

---
+++ health-service/src/main/java/com/gov/zw/patient/DBSeeder.java
@@ -0,0 +1,32 @@
+package com.gov.zw.patient;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
+
+@Component
+public class DBSeeder implements CommandLineRunner {
+
+    private final PatientRepository patientRepository;
+
+    public DBSeeder(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Flux<Patient> patients =
+                Flux
+                        .just(
+                                new Patient("MUZAN123", "Artemas", "Muzanenhamo",
+                                        LocalDate.of(1990, 3, 28),
+                                        "68 Jeremy Street, London, W1 7AA")
+                        );
+
+        patientRepository.deleteAll();
+
+        patientRepository.saveAll(patients);
+    }
+}
