a1965c1f3a63e36b00e3b45951e06bf52265dc75
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2018-11-02 08:34:48 +0000

    add more cases for Doctor object

--- health-service/src/test/java/com/gov/zw/doctor/DoctorTest.java
+++ health-service/src/test/java/com/gov/zw/doctor/DoctorTest.java
@@ -3,7 +3,8 @@ package com.gov.zw.doctor;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,15 +14,20 @@ public class DoctorTest {
 
     @Before
     public void setup() {
-        doctor = new Doctor("Mark", "Smith", Arrays.asList("Surgery"), "DR1234");
+        doctor = new Doctor("Mark", "Smith", Collections.singletonList("Surgery"), "DR1234");
     }
 
     @Test
     public void shouldReturnAllDoctorDetails() {
-        Doctor expectedDoctor = new Doctor("Mark", "Smith", Arrays.asList("Surgery"), "DR1234");
+        List<String> expectedPractices = Collections.singletonList("Surgery");
+
+        Doctor expectedDoctor = new Doctor("Mark", "Smith", Collections.singletonList("Surgery"), "DR1234");
+
         assertThat(doctor).isEqualTo(expectedDoctor);
         assertThat(doctor.getName()).isEqualTo(expectedDoctor.getName());
         assertThat(doctor.getSurname()).isEqualTo(expectedDoctor.getSurname());
+        assertThat(doctor.getPractices()).isEqualTo(expectedDoctor.getPractices());
+        assertThat(doctor.getPracticeLicenseRef()).isEqualTo(expectedDoctor.getPracticeLicenseRef());
     }
 
 }
