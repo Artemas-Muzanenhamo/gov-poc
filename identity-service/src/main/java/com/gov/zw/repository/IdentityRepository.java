560fe1276c023784f5f726a30f2d23112da4f1ca
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2018-12-21 07:42:03 +0000

    Add cross origin to identity repository to access custom queries from spring data

--- identity-service/src/main/java/com/gov/zw/repository/IdentityRepository.java
+++ identity-service/src/main/java/com/gov/zw/repository/IdentityRepository.java
@@ -4,9 +4,11 @@ import com.gov.zw.domain.Identity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource
 public interface IdentityRepository extends MongoRepository<Identity, String> {
 
