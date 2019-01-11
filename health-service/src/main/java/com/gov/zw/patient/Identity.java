373dbc2098bf1c12e9ceaa4e8973e9d5a86742f5
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-01-11 09:34:13 +0000

    add identity client for health service

---
+++ health-service/src/main/java/com/gov/zw/patient/Identity.java
@@ -0,0 +1,20 @@
+package com.gov.zw.patient;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Identity {
+
+    private String id;
+    private String identityRef;
+    private String name;
+    private String surname;
+    private String birthDate;
+    private String villageOfOrigin;
+    private String placeOfBirth;
+    private String dateOfIssue;
+}
---
+++ health-service/src/main/java/com/gov/zw/patient/IdentityClient.java
@@ -0,0 +1,17 @@
+package com.gov.zw.patient;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+
+@FeignClient(name = "identity-service")
+@Component
+public interface IdentityClient {
+
+    @PostMapping(value = "/identities/reference")
+    Identity findIdentityByIdReferenceNumber(@RequestBody Map<String, String> referenceNumber);
+}
