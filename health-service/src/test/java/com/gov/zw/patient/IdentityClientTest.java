16611a4f34bde662e0aa21ec5ccd37d6b15569d3
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-01-17 18:44:38 +0000

    Amend IdentityClientTest for Health-Service service

--- health-service/src/test/java/com/gov/zw/patient/IdentityClientTest.java
+++ health-service/src/test/java/com/gov/zw/patient/IdentityClientTest.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
