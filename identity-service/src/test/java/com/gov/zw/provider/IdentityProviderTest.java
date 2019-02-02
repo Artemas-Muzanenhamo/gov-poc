b42de34612490d1cfb078aa6a3b20a74999f239d
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-02 11:33:59 +0000

    Add health service to provider state

--- identity-service/src/test/java/com/gov/zw/provider/IdentityProviderTest.java
+++ identity-service/src/test/java/com/gov/zw/provider/IdentityProviderTest.java
@@ -50,22 +50,6 @@ public class IdentityProviderTest {
         );
     }
 
-//    @State("identity details from the Health Service client")
-//    public void healthServiceResponse() {
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        Identity identity = new Identity("1", "1", "Artemas", "Muzanenhamo",
-//                "28/03/1990", "Mashayamombe", "Harare", "22/01/2018");
-//        Map<String, String> id = objectMapper.convertValue(identity, Map.class);
-//        JSONObject jsonObject = new JSONObject(id);
-//
-//        embeddedService.addExpectation(
-//                onRequestTo(IDENTITIES_REFERENCE)
-//                        .withMethod(ClientDriverRequest.Method.POST),
-//                giveResponse(jsonObject.toJSONString(), MediaType.APPLICATION_JSON_UTF8_VALUE)
-//        );
-//    }
-
     @TestTarget
     public final Target target = new HttpTarget(8333);
 }
