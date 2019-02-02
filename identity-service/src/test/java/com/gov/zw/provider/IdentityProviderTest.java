abc2a4aa0708c1cc490f18a24ed46c6fec94c899
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-02 12:23:27 +0000

    amend Identity-Service provider method

--- identity-service/src/test/java/com/gov/zw/provider/IdentityProviderTest.java
+++ identity-service/src/test/java/com/gov/zw/provider/IdentityProviderTest.java
@@ -35,7 +35,7 @@ public class IdentityProviderTest {
             "an identity reference number from License Service client",
             "identity details from the Health Service client"
     })
-    public void licenseServiceResponse() {
+    public void returnIdentityDetailsState() {
 
         ObjectMapper objectMapper = new ObjectMapper();
         Identity identity = new Identity("1", "1", "Artemas", "Muzanenhamo",
