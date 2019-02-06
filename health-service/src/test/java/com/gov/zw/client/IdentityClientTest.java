b985d37ac44dc6e78ef2585b7d586558d5889c10
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-06 11:50:35 +0000

    amend Health-Service IdentityClientTest

--- health-service/src/test/java/com/gov/zw/client/IdentityClientTest.java
+++ health-service/src/test/java/com/gov/zw/client/IdentityClientTest.java
@@ -1,16 +1,12 @@
 package com.gov.zw.client;
 
 import au.com.dius.pact.consumer.Pact;
-import au.com.dius.pact.consumer.PactProviderRuleMk2;
 import au.com.dius.pact.consumer.PactVerification;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.model.RequestResponsePact;
 import net.minidev.json.JSONObject;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,19 +18,11 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class IdentityClientTest {
+public class IdentityClientTest extends CDCIdentityClientBaseTest {
 
 
     private static final String IDENTITIES_REFERENCE = "/identities/reference";
     private static final String APPLICATION_JSON_UTF_8_VALUE = "application/json;charset=utf-8";
-
-    @Rule
-    public PactProviderRuleMk2 stubProvider =
-            new PactProviderRuleMk2("identity-service", "localhost", 9999, this);
-
-    @Autowired
-    private IdentityClient identityClient;
 
     @Pact(state = "an identity for a patient", provider = "identity-service", consumer = "health-service")
     public RequestResponsePact retrievePatientIdentityPact(PactDslWithProvider builder) {
