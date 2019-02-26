a3f15a26ec0b113fd120f6679bd6e8b41f2312b5
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-26 19:20:06 +0000

    Add Health-Service to be discoverable with Eureka

--- health-service/src/main/java/com/gov/zw/HealthServiceApplication.java
+++ health-service/src/main/java/com/gov/zw/HealthServiceApplication.java
@@ -4,6 +4,7 @@ import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
@@ -13,6 +14,7 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @EnableFeignClients
 @EnableReactiveMongoRepositories
 @SpringBootApplication
+@EnableEurekaClient
 public class HealthServiceApplication extends AbstractReactiveMongoConfiguration {
 
 	public static void main(String[] args) {
