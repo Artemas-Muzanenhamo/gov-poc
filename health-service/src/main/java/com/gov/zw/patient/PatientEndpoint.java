96b15248d1794e740ab462848eb98f2a8e9563fc
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-03-08 00:10:35 +0000

    Amend PatientEndpoint

--- health-service/src/main/java/com/gov/zw/patient/PatientEndpoint.java
+++ health-service/src/main/java/com/gov/zw/patient/PatientEndpoint.java
@@ -1,10 +1,10 @@
 package com.gov.zw.patient;
 
+import org.reactivestreams.Publisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
@@ -22,9 +22,10 @@ public class PatientEndpoint {
         ).andRoute(
                 PUT("/patients").and(accept(APPLICATION_JSON).and(contentType(APPLICATION_JSON))),
                 request -> {
-                    Mono<Patient> patientMono = request.bodyToMono(Patient.class);
-                    request.bodyToMono(Patient.class);
-                    return ok().build();
+                    // TODO: Create publisher
+                    Publisher<Patient> patientPublisher = null;
+                    return ok().body(patientPublisher, Patient.class);
+//                    return ok().build();
                 });
     }
 }
