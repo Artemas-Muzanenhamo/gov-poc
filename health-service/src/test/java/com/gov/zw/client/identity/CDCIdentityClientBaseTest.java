9c42553abd6cdaa88eea088999a37652e08dcc50
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-03-11 21:56:46 +0000

    Polish up

--- health-service/src/test/java/com/gov/zw/client/identity/CDCIdentityClientBaseTest.java
+++ health-service/src/test/java/com/gov/zw/client/identity/CDCIdentityClientBaseTest.java
@@ -1,7 +1,6 @@
 package com.gov.zw.client.identity;
 
 import au.com.dius.pact.consumer.PactProviderRuleMk2;
-import com.gov.zw.client.identity.IdentityClient;
 import org.junit.Rule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
--- health-service/src/test/java/com/gov/zw/patient/PatientEndpointTest.java
+++ health-service/src/test/java/com/gov/zw/patient/PatientEndpointTest.java
@@ -2,7 +2,6 @@ package com.gov.zw.patient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
