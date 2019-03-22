e301b618e9fada5988c3b45c2dab6d5b19a00ee6
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-03-22 00:01:45 +0000

    Add IdentityJson

---
+++ identity-service/src/main/java/com/gov/zw/domain/IdentityJson.java
@@ -0,0 +1,28 @@
+package com.gov.zw.domain;
+
+public class IdentityJson {
+
+    public String id;
+    public String identityRef;
+    public String name;
+    public String surname;
+    public String birthDate;
+    public String villageOfOrigin;
+    public String placeOfBirth;
+    public String dateOfIssue;
+
+    public IdentityJson() {
+    }
+
+    public IdentityJson(Identity identity) {
+        this.id = identity.getId();
+        this.identityRef = identity.getIdentityRef();
+        this.name = identity.getName();
+        this.surname = identity.getSurname();
+        this.birthDate = identity.getVillageOfOrigin();
+        this.placeOfBirth = identity.getPlaceOfBirth();
+        this.dateOfIssue = identity.getDateOfIssue();
+    }
+
+
+}
