45219eb10c5c944c4f09371972d579e86215aaba
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-04-11 18:24:30 +0100

    Amend IdentityReferenceJsonMapper

--- license-service/src/main/java/com/gov/zw/client/IdentityReferenceJsonMapper.java
+++ license-service/src/main/java/com/gov/zw/client/IdentityReferenceJsonMapper.java
@@ -1,5 +1,8 @@
 package com.gov.zw.client;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class IdentityReferenceJsonMapper {
     public String toIdentityReference(IdentityReferenceJson json) {
         return json.ref;
