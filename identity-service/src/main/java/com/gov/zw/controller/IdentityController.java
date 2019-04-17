9cdadb1744c9e6e7d7b05605b507b47bd40503c0
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-04-17 21:11:04 +0100

    polish up

--- identity-service/src/main/java/com/gov/zw/controller/IdentityController.java
+++ identity-service/src/main/java/com/gov/zw/controller/IdentityController.java
@@ -11,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.springframework.http.HttpStatus.OK;
 
