dd435c9b28b15894206d4200be87fb0b0cef9fe8
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-04-25 20:20:18 +0100

    polish up

--- license-service/src/test/java/com/gov/zw/domain/LicenseJsonTest.java
+++ license-service/src/test/java/com/gov/zw/domain/LicenseJsonTest.java
@@ -11,12 +11,12 @@ class LicenseJsonTest {
     private LicenseJson licenseJson;
 
     @BeforeEach
-    public void init() {
+    void init() {
         licenseJson = new LicenseJson();
     }
 
     @Test
-    public void equality_check() {
+    void equality_check() {
         LicenseJson expectedLicenseJson = new LicenseJson(new License("MUZAN1234", "121", "Muzanenhamo", "Artemas",
                 "28/03/1990", "United Kingdom", "28/03/2010",
                 "28/03/2060", "DVLA", "MUZANK9843ACTK", "001.jpg",
