5926284ddb3ad0e5dc4557d4bfc6ffc2339086a9
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-06-10 18:46:19 +0100

    change tests to use Junit 5

--- health-service/src/test/java/com/gov/zw/patient/PatientServiceUnitTest.java
+++ health-service/src/test/java/com/gov/zw/patient/PatientServiceUnitTest.java
@@ -3,7 +3,6 @@ package com.gov.zw.patient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
--- health-service/src/test/java/com/gov/zw/patient/PatientTest.java
+++ health-service/src/test/java/com/gov/zw/patient/PatientTest.java
@@ -1,29 +1,29 @@
 package com.gov.zw.patient;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class PatientTest {
+class PatientTest {
 
     private Patient patient;
 
-    @Before
-    public void setup() {
+    @BeforeEach
+    void setup() {
         patient = new Patient();
     }
 
     @Test
-    public void shouldReturnEmptyPatientObject() {
+    void shouldReturnEmptyPatientObject() {
         Patient patientDummy = new Patient();
         assertThat(patient).isEqualTo(patientDummy);
     }
 
     @Test
-    public void shouldReturnPatientObjectPopulated() {
+    void shouldReturnPatientObjectPopulated() {
         patient = new Patient("MUZAN123", "Artemas", "Muzanenhamo", LocalDate.of(1990, 3, 28),
                  "68 Jeremy Street, London, W1 7AA");
 
