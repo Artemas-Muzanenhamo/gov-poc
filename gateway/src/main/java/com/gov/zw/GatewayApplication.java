8610534c8bc6e7453d177d41d8605f26b132504e
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-07-04 22:46:43 +0100

    Amend Eureka Client dependecies

--- gateway/src/main/java/com/gov/zw/GatewayApplication.java
+++ gateway/src/main/java/com/gov/zw/GatewayApplication.java
@@ -3,11 +3,9 @@ package com.gov.zw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableZuulProxy
 public class GatewayApplication {
