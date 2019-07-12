614fe3312343fb3a19b4a3d501cfb3acbe2133a5
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-07-12 22:46:02 +0100

    Amend IdentityProvider test

--- identity-service/src/test/java/com/gov/zw/provider/IdentityProviderTest.java
+++ identity-service/src/test/java/com/gov/zw/provider/IdentityProviderTest.java
@@ -1,5 +1,6 @@
 package com.gov.zw.provider;
 
+import au.com.dius.pact.provider.PactVerifyProvider;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.loader.PactFolder;
@@ -10,12 +11,21 @@ import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.restdriver.clientdriver.ClientDriverRule;
+import com.gov.zw.IdentityServiceApplication;
 import com.gov.zw.domain.Identity;
+import com.gov.zw.domain.IdentityJson;
+import com.gov.zw.domain.IdentityReferenceJson;
+import com.gov.zw.service.IdentityService;
+import com.gov.zw.util.InvalidIdentityReferenceException;
 import net.minidev.json.JSONObject;
-import org.junit.ClassRule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.lanwen.wiremock.ext.WiremockResolver;
 import ru.lanwen.wiremock.ext.WiremockUriResolver;
 
@@ -26,17 +36,23 @@ import java.util.Map;
 import static com.github.restdriver.clientdriver.ClientDriverRequest.Method.POST;
 import static com.github.restdriver.clientdriver.RestClientDriver.giveResponse;
 import static com.github.restdriver.clientdriver.RestClientDriver.onRequestTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @Provider("identity-service")
 @PactFolder("../pacts")
-@ExtendWith({
-        WiremockResolver.class,
-        WiremockUriResolver.class
-})
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = IdentityServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+        properties = "server.port=8080")
 public class IdentityProviderTest {
 
+    @MockBean
+    private IdentityService identityService;
+
     private static final String IDENTITIES_REFERENCE = "/identities/reference";
+
+    @LocalServerPort private int serverPort;
 
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
@@ -45,20 +61,22 @@ public class IdentityProviderTest {
     }
 
     @BeforeEach
-    void before(PactVerificationContext context) throws MalformedURLException {
-        context.setTarget(HttpTestTarget.fromUrl(new URL("http://localhost:8080")));
-//         context.setTarget(new HttpTestTarget("localhost", 8333, "/"));
+    void setTarget(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", serverPort));
     }
-
-//    @ClassRule
-//    public static final ClientDriverRule embeddedService = new ClientDriverRule(8333);
 
     @State({
             "an identity reference number from License Service client",
             "identity details from the Health Service client"
     })
-    public void identityDetailsState() {
+    public void identityDetailsState() throws Exception {
 
+        Identity identity = new Identity("1", "1", "Artemas", "Muzanenhamo",
+                "28/03/1990", "Mashayamombe", "Harare", "22/01/2018");
+        IdentityJson identityJson = new IdentityJson(identity);
+
+        when(identityService.findIdentityByIdentityRef(any(IdentityReferenceJson.class))).thenReturn(identityJson);
+//
 //        ObjectMapper objectMapper = new ObjectMapper();
 //        Identity identity = new Identity("1", "1", "Artemas", "Muzanenhamo",
 //                "28/03/1990", "Mashayamombe", "Harare", "22/01/2018");
@@ -71,7 +89,4 @@ public class IdentityProviderTest {
 //                giveResponse(identityJsonObject.toJSONString(), APPLICATION_JSON_UTF8_VALUE)
 //        );
     }
-
-//    @TestTarget
-//    public final Target target = new HttpTarget(8333);
 }
