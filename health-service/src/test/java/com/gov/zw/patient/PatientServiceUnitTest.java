eba7e70ebc7edfcd3ad5ed09a24d8f6a6e69604f
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-10-04 13:45:04 +0100

    Polish up PatientServiceUnitTest

--- health-service/src/test/java/com/gov/zw/patient/PatientServiceUnitTest.java
+++ health-service/src/test/java/com/gov/zw/patient/PatientServiceUnitTest.java
@@ -20,6 +20,11 @@ import static reactor.core.publisher.Mono.just;
 @ExtendWith(MockitoExtension.class)
 class PatientServiceUnitTest {
 
+    private static final String IDENTITY_REF = "MUZAN123";
+    private static final String NAME = "Artemas";
+    private static final String SURNAME = "Thomas";
+    private static final LocalDate DATE_OF_BIRTH = LocalDate.of(1990, 3, 28);
+    private static final String ADDRESS = "123 Rock Street, London, W1 7XX";
     private PatientServiceImpl patientServiceImpl;
 
     @Mock
@@ -33,9 +38,7 @@ class PatientServiceUnitTest {
 
     @Test
     void should_return_all_patients() {
-        Patient patient = new Patient("MUZAN123", "Artemas", "Muzanenhamo",
-                LocalDate.of(1990, 3, 28),
-                "68 Jeremy Street, London, W1 7AA");
+        Patient patient = new Patient(IDENTITY_REF, NAME, SURNAME, DATE_OF_BIRTH, ADDRESS);
         given(patientRepository.findAll()).willReturn(Flux.just(patient));
 
         Flux<Patient> patientsFlux = patientServiceImpl.getAllPatients();
@@ -48,9 +51,7 @@ class PatientServiceUnitTest {
 
     @Test
     void should_add_patient_details_given_a_valid_id() {
-        Patient patient = new Patient("MUZAN123", "Artemas", "Muzanenhamo",
-                LocalDate.of(1990, 3, 28),
-                "68 Jeremy Street, London, W1 7AA");
+        Patient patient = new Patient(IDENTITY_REF, NAME, SURNAME, DATE_OF_BIRTH, ADDRESS);
         given(patientRepository.insert(patient)).willReturn(just(patient));
 
         Mono<Patient> patientMono = patientServiceImpl.addPatient(patient);
@@ -63,9 +64,7 @@ class PatientServiceUnitTest {
 
     @Test
     void should_update_patient_details() {
-        Patient patient = new Patient("MUZAN123", "Artemas", "Thomas",
-                LocalDate.of(1990, 3, 28),
-                "123 Rock Street, London, W1 7XX");
+        Patient patient = new Patient(IDENTITY_REF, NAME, SURNAME, DATE_OF_BIRTH, ADDRESS);
         when(patientRepository.save(patient)).thenReturn(just(patient));
 
         Mono<Patient> patientMono = patientServiceImpl.updatePatient(patient);
@@ -78,9 +77,7 @@ class PatientServiceUnitTest {
 
     @Test
     void should_retrieve_a_single_patient_details() {
-        Patient patient = new Patient("MUZAN123", "Artemas", "Thomas",
-                LocalDate.of(1990, 3, 28),
-                "123 Rock Street, London, W1 7XX");
+        Patient patient = new Patient(IDENTITY_REF, NAME, SURNAME, DATE_OF_BIRTH, ADDRESS);
         Optional<String> identityRefOptional = Optional.of(patient.getIdentityRef());
         given(patientRepository.findById(patient.getIdentityRef())).willReturn(just(patient));
 
