af136b1fc689a7b6d81c569bddcb182d5d106784
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-10-28 08:14:02 +0000

    Add test scenario for updating an identity

--- identity-service/src/test/java/com/gov/zw/controller/IdentityControllerUnitTest.java
+++ identity-service/src/test/java/com/gov/zw/controller/IdentityControllerUnitTest.java
@@ -61,8 +61,28 @@ class IdentityControllerUnitTest {
 
         identityController.saveIdentity(identityJson);
 
-        Mockito.verify(identityServiceImpl).save(identity);
+        verify(identityServiceImpl).save(identity);
+    }
 
+    @Test
+    @DisplayName("Should update an identity given a valid identity")
+    void updateIdentity() throws Exception {
+        Identity identity = new Identity(ID, IDENTITY_REF, NAME, SURNAME,
+                BIRTH_DATE, VILLAGE_OF_ORIGIN,
+                PLACE_OF_BIRTH, DATE_OF_ISSUE);
+        IdentityJson identityJson = new IdentityJson(
+                identity.getId(),
+                identity.getIdentityRef(),
+                identity.getName(),
+                identity.getSurname(),
+                identity.getBirthDate(),
+                identity.getVillageOfOrigin(),
+                identity.getPlaceOfBirth(),
+                identity.getDateOfIssue());
+
+        identityController.updateIdentity(identityJson);
+
+        verify(identityServiceImpl).save(identity);
     }
 
     @Test
