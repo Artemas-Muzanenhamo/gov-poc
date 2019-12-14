1c9c2125f46a70e1817a110d7d755365e044363a
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-12-14 00:17:45 +0000

    Add swagger to identity service and license service

--- health-service/src/main/java/com/gov/zw/HealthServiceApplication.java
+++ health-service/src/main/java/com/gov/zw/HealthServiceApplication.java
@@ -1,13 +1,10 @@
 package com.gov.zw;
 
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
@@ -17,19 +14,9 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
-public class HealthServiceApplication extends AbstractReactiveMongoConfiguration {
+public class HealthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthServiceApplication.class, args);
-	}
-
-	@Override
-	public MongoClient reactiveMongoClient() {
-		return MongoClients.create();
-	}
-
-	@Override
-	protected String getDatabaseName() {
-		return "health-service-db";
 	}
 }
