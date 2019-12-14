f443a74cc3953a74a4260c7afa897da842d5f72b
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-12-14 00:35:42 +0000

    fix mongo configuration

--- health-service/src/main/java/com/gov/zw/HealthServiceApplication.java
+++ health-service/src/main/java/com/gov/zw/HealthServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
 @EnableFeignClients
-@EnableReactiveMongoRepositories
 @EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
