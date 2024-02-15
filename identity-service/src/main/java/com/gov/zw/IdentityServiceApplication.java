825f1e2b3f98bbf260179afd3d89179f3212c971
Artemas-Muzanenhamo <artemasseng@gmail.com>
2024-02-15 21:28:01 +0000

    upgrade identity-service

--- identity-service/src/main/java/com/gov/zw/IdentityServiceApplication.java
+++ identity-service/src/main/java/com/gov/zw/IdentityServiceApplication.java
@@ -3,11 +3,11 @@ package com.gov.zw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableEurekaClient
 @SpringBootApplication
-@EnableSwagger2
+//@EnableSwagger2
 public class IdentityServiceApplication {
 
     public static void main(String[] args) {
