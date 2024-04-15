7a0811706ffbf52e8cb526cfdfc284812f25c8d8
Artemas-Muzanenhamo <artemasseng@gmail.com>
2024-04-15 21:08:20 +0100

    upgrade identity-service to Spring Boot 3.2.4

--- identity-service/src/test/java/com/gov/zw/provider/IdentityProviderTest.java
+++ identity-service/src/test/java/com/gov/zw/provider/IdentityProviderTest.java
@@ -1,11 +1,11 @@
 package com.gov.zw.provider;
 
-import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.State;
-import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
 import com.gov.zw.IdentityServiceApplication;
 import com.gov.zw.domain.Identity;
 import com.gov.zw.domain.IdentityReference;
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -38,7 +38,8 @@ class IdentityProviderTest {
     @MockBean
     private IdentityService identityService;
 
-    @LocalServerPort private int serverPort;
+    @LocalServerPort
+    private int serverPort;
 
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
